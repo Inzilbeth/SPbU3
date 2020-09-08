b47c645ea9df63d555d64b2bf4e674b91b698e73
Inzilbeth <talgat.minkashev@yandex.com>
2020-09-08 18:44:52 +0300

    Added solution basis

---
+++ Homework2/Task1Library/Task1Library/ILazy.cs
@@ -0,0 +1,4 @@
+﻿public interface ILazy<T>
+{
+    T Get();
+}
---
+++ Homework2/Task1Library/Task1Library/LazySingleThreaded.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1Library
+{
+    public class LazySingleThreaded<T> : ILazy<T>
+    {
+        public T Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
