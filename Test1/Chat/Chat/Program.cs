bef44bcb3f41e1b5bf0a972cf123f77b9dce6909
Inzilbeth <talgat.minkashev@yandex.com>
2020-10-27 11:03:10 +0300

    added solution

---
+++ Test1/Chat/Chat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System;
+
+namespace Chat
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            var port = 8888;
+
+            if (args.Length == 1)
+            {
+                var server = new ChatServer(IPAddress.Parse(args[0]), port);
+                await server.Run();
+            }
+            else if (args.Length == 2)
+            {
+                var client = new ChatClient(args[0], Convert.ToInt32(args[1]));
+                await client.Run();
+            }
+        }
+    }
+}
