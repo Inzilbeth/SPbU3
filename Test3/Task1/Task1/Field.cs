ce79165a2b612469fd150cc61adde56105e8bc52
Inzilbeth <talgat.minkashev@yandex.com>
2020-12-15 10:52:49 +0300

    Added solution

---
+++ Test3/Task1/Task1/Field.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class Field
+    {
+        public enum Status
+        {
+            InProgress,
+            CrossWon,
+            ZeroWon,
+            Draw
+        }
+
+        private Cell[,] array;
+        private Status status;
+
+        public Field()
+        {
+            array = new Cell[3, 3];
+            Reset();
+        }
+
+        public Status GetStatus()
+            => status;
+
+        public void Reset()
+        {
+            status = Status.InProgress;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    array[i, j] = Cell.Empty;
+                }
+            }
+        }
+
+        public bool Add(int i, int j, Cell type)
+        {
+            if (type == Cell.Empty ||
+                array[i, j] != Cell.Empty ||
+                !IsOnField(i, j) ||
+                status != Status.InProgress)
+            {
+                return false;
+            }
+            else
+            {
+                array[i, j] = type;
+            }
+
+            UpdateStatus();
+            return true;
+        }
+
+        private bool IsOnField(int i, int j)
+            => i >= 0 && i < 3 && j >= 0 && j < 3;
+
+        private void UpdateStatus()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (array[i, 0] != Cell.Empty && array[i, 0] == array[i, 1] && array[i, 1] == array[i, 2])
+                {
+                    status = array[i, 0] == Cell.X ? Status.CrossWon : Status.ZeroWon;
+                    return;
+                }
+
+                if (array[0, i] != Cell.Empty && array[0, i] == array[1, i] && array[1, i] == array[2, i])
+                {
+                    status = array[0, i] == Cell.X ? Status.CrossWon : Status.ZeroWon;
+                    return;
+                }
+            }
+
+            if (array[0, 0] != Cell.Empty && array[0, 0] == array[1, 1] && array[1, 1] == array[2, 2])
+            {
+                status = array[0, 0] == Cell.X ? Status.CrossWon : Status.ZeroWon;
+                return;
+            }
+
+            if (array[2, 0] != Cell.Empty && array[2, 0] == array[1, 1] && array[1, 1] == array[0, 2])
+            {
+                status = array[2, 0] == Cell.X ? Status.CrossWon : Status.ZeroWon;
+                return;
+            }
+
+            int count = 0;
+            foreach (var cell in array)
+            {
+                if (cell != Cell.Empty)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 9)
+            {
+                status = Status.Draw;
+                return;
+            }
+
+            status = Status.InProgress;
+        }
+    }
+}
---
+++ Test3/Task1/Task1/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class GameManager
+    {
+        private Field field;
+        private Cell currentSign;
+
+        public GameManager()
+        {
+            field = new Field();
+            currentSign = Cell.X;
+        }
+
+        public string GetSign()
+            => currentSign.ToString();
+
+        public void Reset()
+        {
+            field.Reset();
+            currentSign = Cell.X;
+        }
+
+        public bool TryMakeTurn(int i, int j)
+        {
+            if (field.Add(i, j, currentSign))
+            {
+                ChangeSign();
+                return true;
+            }
+
+            return false;
+        }
+
+        public Field.Status GetStatus()
+            => field.GetStatus();
+
+        private void ChangeSign()
+        {
+            currentSign = currentSign == Cell.X ? Cell.O : Cell.X;
+        }
+    }
+}
